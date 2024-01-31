c1bd8b59447e859036f75a7e178351d3e0e78bf5
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-31 18:27:23 +0100

    create card con inserimento di contenuti

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,18 +4,19 @@
     <section class="comics">
         <div class="container">
             <div class="row">
-                @foreach ($comics as $comic)
-                    
+                @foreach ($fumetti as $fumetto)
+
                     <div class="col-2">
-                        <div class="card" style="width: 18rem;">
-                            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+                        <div class="card">
+                            <img src="{{ $fumetto['thumb'] }}" class="card-img-top" alt="...">
                             <div class="card-body">
-                            <h5 class="card-title">
-                                {{ $comic['title']}}
-                            </h5>
+                                <h5 class="card-title">
+                                    {{ $fumetto['series']}}
+                                </h5>
                             </div>
                         </div>
                     </div>
+
                 @endforeach
 
             </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
+    $fumetti = config('comics.fumetti');
+    return view('pages.home', ['fumetti' => $fumetti]);
 });
